fd67766e74503024e1a735557f61863bc79e8ad4
Guillermo Alejandro Martín Carretero <guillermo@iMac-de-Guillermo.local>
2024-12-25 18:43:38 +0100

    Added latest video to homeopage

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req,res) => {
-    res.render("index.ejs");
-});
+app.get("/", async (req,res) => {
+    try {
+        const result = await axios.get("https://www.scorebat.com/video-api/v3/feed/?token=MTkxNjgzXzE3MzQ5Njk4NjJfOTU5MTM3NDU0MjYxMDk2YzQ0YWZlYTg2MTRhOGIzNWMzNGFiZWY5Nw==");
+        res.render("index.ejs", {
+            result
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+    }});
 
 app.get("/all", async (req,res) => {
     try {
@@ -31,7 +38,7 @@ app.post("/submit", async (req, res) => {
 
       console.log(result.data.response[0]);
       console.log(req.body["comps"]);
-      
+
       res.render("competition.ejs", {
         comp,
         result
